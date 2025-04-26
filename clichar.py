4dc907505eeb3223d818cc15ae880eb1b03140b7
mercuridi <kaibarberharris@protonmail.com>
2025-04-26 15:15:00 +0100

    basic function

--- clichar.py
+++ clichar.py
@@ -1,9 +1,6 @@
 
 def input_stat(stat_name):
-    
-    
-def input_string(request):
-    pass
+    return input_int(f"Please input your {stat_name} score: ", 20, 8)
 
 def input_int(request: str, upper=float("inf"), lower=float("-inf")):
     while True:
@@ -21,18 +18,21 @@ def check_bounds(value: int, upper, lower):
         check = out_of_bounds_check(value, "high", upper)
     elif value < lower:
         check = out_of_bounds_check(value, "low", lower)
-    else: check == True
+    else: 
+        check = True
     return check
 
 def out_of_bounds_check(value: int, hi_lo: str, exceeded_bound: int):
     if hi_lo == "high":
         maxmin_str = "max"
+    else:
+        maxmin_str = "min"
     print(f"The value ({value}) is {hi_lo}er than expected.")
     print(f"The {maxmin_str}imum expected value was {exceeded_bound}.")
     if input_bool("Would you like to continue anyway? "):
         return True
     else:
-        False
+        return False
 
 def input_bool(request: str):
     while True:
@@ -44,13 +44,23 @@ def input_bool(request: str):
         else:
             print("Please enter an affirmative or negative reply.")
             print("Empty replies are assumed negative.")
-        
+
 class Stat:
     def __init__(self, name, value):
         self.name = name
-        self.abbreviation = name[:2].upper()
+        self.abbreviation = name[:3].upper()
         self.value = value
         self.modifier = (value - 10) // 2
+    
+    def __str__(self):
+        if self.modifier > 0:
+            mod_string = f"+{self.modifier} "
+        else:
+            mod_string = str(self.modifier)
+        return f"{self.name} " + \
+            f"({self.abbreviation}): " + \
+            f"({self.value} " + \
+            f"({mod_string}) "
 
 class Skill:
     def __init__(self, name: str, parent_stat: Stat):
@@ -59,38 +69,44 @@ class Skill:
 
 class Skills:
     def __init__(self):
-        self.acrobatics = Skill("Acrobatics")
+        pass
         
 class Character:
     def __init__(self):
         print("Character creator initialised...")
-        name = input("Please enter your character's name: ")
+        self.name = input("Please enter your character's name: ").title()
         print("Next, we will define your character's stats.")
         print("This number typically ranges from 8 to 18, ")
         print("but can be as low as 1 and as high as 30.")
         
-
-        # character stats
-        self.strength = Stat("Strength",
-            input_int(f"Please enter your character's Strength score: ", 
-                        upper = 30, lower = 1)
-        self.dexterity
-        self.constitution
-        self.intelligence
-        self.wisdom
-        self.charisma
+        # character race
         
-    def __str__(self):
-        pass
-    
-    def set_str(self, value: int):
-        self.str = value
+        # character stats
+        self.strength       = Stat("Strength"    , input_stat("Strength"))
+        self.dexterity      = Stat("Dexterity"   , input_stat("Dexterity"))
+        self.constitution   = Stat("Constitution", input_stat("Constitution"))
+        self.intelligence   = Stat("Intelligence", input_stat("Intelligence"))
+        self.wisdom         = Stat("Wisdom"      , input_stat("Wisdom"))
+        self.charisma       = Stat("Charisma"    , input_stat("Charisma"))
 
+    def print_name(self):
+        print(f"Character name: {self.name}")
 
-
+    def print_stats(self):
+        print(f"{self.strength}")
+        print(f"{self.dexterity}")
+        print(f"{self.constitution}")
+        print(f"{self.intelligence}")
+        print(f"{self.wisdom}")
+        print(f"{self.charisma}")
+        
+def print_char(char: Character):
+    char.print_name()
+    char.print_stats()
 
 def main():
     new_char = Character()
+    print_char(new_char)
 
 if __name__ == "__main__":
     main()
