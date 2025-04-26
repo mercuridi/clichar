591760a38c5781b47145e5b128f7b0704b15e2ef
mercuridi <kaibarberharris@protonmail.com>
2025-04-26 12:19:18 +0100

    first commit

---
+++ clichar.py
@@ -0,0 +1,34 @@
+class Character:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+    
+    def set_str(self, val: int):
+        self.str = val
+
+
+def main():
+    pass
+
+
+
+
+
+def input_string(request):
+    pass
+
+def input_int(request):
+    pass
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
